444bbd54905ed8103e438802cb1c4d0d580eb1d2
Niladri <niladrisengit12@gmail.com>
2023-10-09 02:23:02 +0530

    day-11

--- fullstack/client/screens/home/Home.jsx
+++ fullstack/client/screens/home/Home.jsx
@@ -4,6 +4,7 @@ import tw from "twrnc";
 import { LinearGradient } from "expo-linear-gradient";
 import { AuthContext } from "../../context/authContext";
 import Footer from "../Menus/Footer";
+import Header from "../Menus/Header";
 
 const Home = () => {
   const [user, setUser] = useContext(AuthContext);
@@ -12,11 +13,14 @@ const Home = () => {
   return (
     <LinearGradient
       colors={["#00008B", "#1F75FE"]}
-      style={tw` flex-1 relative items-center justify-center `}
+      style={tw` flex-1 relative  `}
       start={{ x: -0.7, y: 0.5 }}
       end={{ x: 0.7, y: 0.9 }}
     >
-      <Text style={tw`text-white font-bold text-4xl`}>
+      <View style={tw`absolute top-0 w-full z-50`}>
+        <Header />
+      </View>
+      <Text style={tw`text-white font-bold text-2xl py-26 px-4`}>
         Welcome {loggedinUser}
       </Text>
       <View style={tw`absolute bottom-0 w-full`}>
