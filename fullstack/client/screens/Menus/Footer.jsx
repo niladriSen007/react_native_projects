90c38ead02d01a953a76a0fb1dcb988e9253ab72
Niladri <niladrisengit12@gmail.com>
2023-10-09 23:44:27 +0530

    day-12

--- fullstack/client/screens/Menus/Footer.jsx
+++ fullstack/client/screens/Menus/Footer.jsx
@@ -1,8 +1,10 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 
 const Footer = () => {
+  const navigateTo = useNavigation();
   return (
     <View style={tw`bg-white py-2 px-2 `}>
       <View style={tw`flex-row justify-between px-4 `}>
@@ -14,12 +16,12 @@ const Footer = () => {
           <Text style={tw`text-blue-500 text-2xl pb-1`}>➕</Text>
           <Text style={tw`text-blue-600 text-base`}>Post</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center justify-center`}>
+        <TouchableOpacity style={tw`flex items-center justify-center`}  onPress={()=>navigateTo.navigate("about")}>
           {/* <FontAwesome name="info-circle" size={24} color="blue" /> */}
           <Text style={tw`text-blue-500 text-2xl pb-1`}>ℹ️</Text>
           <Text style={tw`text-blue-600 text-base`}>About</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center justify-center`}>
+        <TouchableOpacity style={tw`flex items-center justify-center`}  onPress={()=>navigateTo.navigate("account")}>
           <Text style={tw`text-blue-500 text-2xl pb-1`}>👤</Text>
           <Text style={tw`text-blue-600 text-base`}>Account</Text>
         </TouchableOpacity>
