e8ab928a6860c8b187afc939db9af28f274d5b86
Niladri <niladrisengit12@gmail.com>
2023-10-10 21:53:28 +0530

    day-14

--- fullstack/client/screens/Account.jsx
+++ fullstack/client/screens/Account.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Image } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import tw from "twrnc";
 import { LinearGradient } from "expo-linear-gradient";
 import Header from "./Menus/Header";
@@ -8,13 +8,15 @@ import { AuthContext } from "../context/authContext";
 
 const Account = () => {
   const [user, setUser] = useContext(AuthContext);
-  console.log("User " + user?.user?.userName);
+  const [updatePopup, setUpdatePopup] = useState(true);
+
+  console.log(user?.user);
   const loggedinUser = user?.user?.userName;
 
   return (
     <LinearGradient
       colors={["#00008B", "#1F75FE"]}
-      style={tw` flex-1 relative  `}
+      style={tw` flex-1 relative `}
       start={{ x: -0.7, y: 0.5 }}
       end={{ x: 0.7, y: 0.9 }}
     >
@@ -28,12 +30,13 @@ const Account = () => {
           }}
           style={tw`w-36 h-36 rounded-full border-4 border-white p-4 `}
         />
-        <Text style={tw`text-3xl py-3 pb-1 font-bold text-white`}>
-          {loggedinUser}
-        </Text>
-        <Text style={tw`text-2xl  text-white`}>
-          Full Stack Developer
-        </Text>
+        <View style={tw`flex flex-row items-center gap-2`}>
+          <Text style={tw`text-3xl py-3 pb-1 font-bold text-white`}>
+            {loggedinUser}
+          </Text>
+          <Text style={tw`text-2xl`}>✏️</Text>
+        </View>
+        <Text style={tw`text-2xl  text-white`}>Full Stack Developer</Text>
         <View style={tw`bg-white w-full absolute bottom-18 h-96`}>
           <View
             style={tw`flex flex-row gap-3 items-start bg-blue-700 mx-5 rounded-lg py-4 items-center justify-start my-6 px-2`}
@@ -62,12 +65,16 @@ const Account = () => {
             <Text style={tw`text-5xl`}>📞</Text>
             <View>
               <Text style={tw`text-2xl text-white`}>Contact</Text>
-              <Text style={tw`text-2xl text-white`}>
-                85840712911
-              </Text>
+              <Text style={tw`text-2xl text-white`}>85840712911</Text>
             </View>
           </View>
         </View>
+        {updatePopup && (
+          <View style={tw`absolute bg-black w-32 h-32`}>
+            {" "}
+            <Text>HIIIIIIIIIIIIIIIIIIIIIIII</Text>{" "}
+          </View>
+        )}
       </View>
       <View style={tw`absolute bottom-0 w-full`}>
         <Footer />
--- fullstack/client/screens/Menus/Footer.jsx
+++ fullstack/client/screens/Menus/Footer.jsx
@@ -1,27 +1,28 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import tw from "twrnc";
 
 const Footer = () => {
   const navigateTo = useNavigation();
+  const route = useRoute()
   return (
     <View style={tw`bg-white py-2 px-2 `}>
       <View style={tw`flex-row justify-between px-4 `}>
-        <TouchableOpacity style={tw`flex items-center justify-center`}>
+        <TouchableOpacity style={tw`flex items-center justify-center ${route?.name === "home" && 'bg-blue-100 p-2 rounded-md '}`} onPress={()=>navigateTo.navigate("home")}>
           <Text style={tw`text-blue-500 text-xl pb-1`}>🏠</Text>
           <Text style={tw`text-blue-600 text-base`}>Home</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center justify-center`}>
+        <TouchableOpacity style={tw`flex items-center justify-center ${route?.name === "post" && 'bg-blue-100 p-2 rounded-md '}`} onPress={()=>navigateTo.navigate("post")}>
           <Text style={tw`text-blue-500 text-2xl pb-1`}>➕</Text>
           <Text style={tw`text-blue-600 text-base`}>Post</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center justify-center`}  onPress={()=>navigateTo.navigate("about")}>
+        <TouchableOpacity style={tw`flex items-center justify-center ${route?.name === "about" && 'bg-blue-100 p-2 rounded-md '}`}  onPress={()=>navigateTo.navigate("about")} >
           {/* <FontAwesome name="info-circle" size={24} color="blue" /> */}
           <Text style={tw`text-blue-500 text-2xl pb-1`}>ℹ️</Text>
           <Text style={tw`text-blue-600 text-base`}>About</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={tw`flex items-center justify-center`}  onPress={()=>navigateTo.navigate("account")}>
+        <TouchableOpacity style={tw`flex items-center justify-center ${route?.name === "account" && 'bg-blue-100 py-2 px-1 rounded-md '}`}  onPress={()=>navigateTo.navigate("account")} >
           <Text style={tw`text-blue-500 text-2xl pb-1`}>👤</Text>
           <Text style={tw`text-blue-600 text-base`}>Account</Text>
         </TouchableOpacity>
